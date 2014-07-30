59a57b50a7584b85de7718757060728d5c9bfb38
Andreas Kloeckner <inform@tiker.net>
2014-07-30 17:49:46 -0500

    Adapt to new page API

--- code/simple_questions.py
+++ code/simple_questions.py
@@ -66,7 +66,7 @@ class MyTextQuestion(PageBase):
             raise ValidationError("%s: at least one answer must be provided"
                     % location)
 
-        validate_markup(vctx, location, page_desc.content)
+        validate_markup(vctx, location, page_desc.prompt)
 
         PageBase.__init__(self, vctx, location, page_desc.id)
         self.page_desc = page_desc
